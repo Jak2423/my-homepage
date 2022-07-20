b07c363d6737af18b205678d505b75c91603d243
Javkhlan <javhlan0420@gmail.com>
2022-07-20 13:35:45 +0800

    Nothing

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className='flex flex-col justify-center items-center text-center mb-8 max-w-2xl mx-auto w-full'>
-      <hr className='w-full border-1 border-gray-200 dark:border-gray-800 mb-8' />
       <p className='text-gray-500'>Designed and Developed by Javkhlan.</p>
     </footer>
   );
